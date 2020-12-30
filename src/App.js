f5b656ce0f6560dcfa13eb759da51f846775a011
pranavv481 <pranavv481@gmail.com>
2020-12-30 09:53:22 +0530

    material ui checkbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container, Checkbox, Box, Paper, FormControl, FormControlLabel, Typography} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import React,{useState} from "react";
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    backgroundColor: theme.palette.grey[200],
+    width:"100vw",
+    height:"100vh",
+    paddingTop:theme.spacing(5)
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [remember, setRemember] = useState(false)
+  const handleChange= (e)=>{
+    setRemember(e.target.checked)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={classes.root}  >
+      <Paper component={Box} width="30%" padding={4} mx="auto">
+        <Typography>{remember ? "true":"false"}</Typography>
+        <Box component="form">
+          <FormControl>
+            <FormControlLabel label="keep me remember" control={<Checkbox 
+             icon={<FavoriteBorderIcon/>}
+             checkedIcon={<FavoriteIcon />}
+             onChange={handleChange}
+              checked={remember}/>}/>
+          </FormControl>
+      
+        </Box>
+      </Paper>
+    </Container>
   );
 }
 
